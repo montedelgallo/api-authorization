75f99d06ec4fc4af4850e06a92a449592cfa4850
Azdren Ymeri <azdreny@gmail.com>
2020-08-06 13:20:03 +0200

    changed tests from minitest to rspec

---
+++ spec/api_authorization/api_authorization_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ApiAuthorization do
+
+  describe "should authorize user" do
+    it "should return true" do 
+      expect(true).to eq(true) 
+    end
+  end
+  
+end
--- test/api_authorization_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ApiAuthorization::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, ApiAuthorization
-  end
-end
